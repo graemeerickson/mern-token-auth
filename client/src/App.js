9373abbdaf212fddab4d8575d5776821a29aaa31
graemeerickson <graemeerickson@gmail.com>
2018-05-08 10:15:22 -0700

    Build out navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import axios from 'axios';
-import logo from './logo.svg';
 import './App.css';
 import Home from './Home';
 import Nav from './layout/Nav';
@@ -10,14 +9,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
         <Router>
           <div className="container">
             <Nav />
-            <p>Navbar coming soon</p>
             <Route exact path="/" component={Home} />
           </div>
         </Router>
